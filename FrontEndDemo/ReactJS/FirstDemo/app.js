1e80ff4778ec4b00d77d8b5f3d325fb0a36ad11d
duy.pham <duy.pham@logixtek.com>
2020-11-04 17:06:43 +0700

    Reactjs

---
+++ FrontEndDemo/ReactJS/FirstDemo/app.js
@@ -0,0 +1,17 @@
+ //need adding React | ReactDom | Babel
+ function Person(props){
+     return(
+        <div className="person">
+            <h1>{props.name}</h1>
+            <p>Age: {props.age}</p>
+        </div>
+     )
+ }
+
+var app = (
+<div>
+    <Person name="Brown" age="35" />
+    <Person name="Red" age="45" />
+</div>
+ )
+ ReactDOM.render(app,document.querySelector('#root')); 
