0d94e8b2d4b8acd74894bac8f6ae474e9444a2bc
duy.pham <duy.pham@logixtek.com>
2020-11-05 11:59:32 +0700

    working with app js file

--- FrontEndDemo/ReactJS/react-tutorials/src/App.js
+++ FrontEndDemo/ReactJS/react-tutorials/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //  <h1>This is the first React application</h1>
+    //  <p>Description</p>
+    // </div>
+     //Cant put your UI code outerdiv 
+     //React.createElement('div',{className:'App'},React.createElement('h1',null,'This is the first React app'))
+    <>
+
+        <div className="App">
+            <h1>This is the first React application</h1>
+           
+        </div> 
+            <p>Can put your code here</p>
+    </>
   );
 }
 
