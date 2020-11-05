f53ff9cbacebd8ba1b011b74f9b60c269ac66da8
duy.pham <duy.pham@logixtek.com>
2020-11-05 14:51:17 +0700

    useState

--- FrontEndDemo/ReactJS/react-tutorials/src/App.js
+++ FrontEndDemo/ReactJS/react-tutorials/src/App.js
@@ -1,16 +1,34 @@
-import React,{Component} from "react";
+import React,{useState} from "react";
 import "./App.css";
 import  Person from './components/Person';
+import  Example  from './components/Example';
 
 function App() {
+
+  const [persons,setPersons] = useState([
+    {name:"Red", age:4},
+    {name:"Green", age:43},
+    {name:"Blue", age:452},
+
+  ]);
+  const changeNameHandler = (e)=> {
+    setPersons([
+      {name:"Brown", age:14},
+      {name:"Yellow", age:33},
+      {name:"White", age:52},
+  
+    ]);
+  }
   return (
       <>
 
         <div className="App">
-            <h1>This is the first React application</h1>
-            <Person></Person>
-            <Person/>
-            <Person/>
+            <h1>React application</h1>
+            <button onClick={(e) => changeNameHandler()}>Change Name</button>
+            <Person click={changeNameHandler} name={persons[0].name} age={persons[0].age}></Person>
+            <Person click={changeNameHandler} name={persons[1].name} age={persons[1].age}></Person>
+            <Person click={changeNameHandler} name={persons[2].name} age={persons[2].age}></Person>
+            <Example></Example>
         </div>  
     </>
   );
--- FrontEndDemo/ReactJS/react-tutorials/src/components/Person.js
+++ FrontEndDemo/ReactJS/react-tutorials/src/components/Person.js
@@ -1,15 +1,20 @@
 import React ,{Component} from "react";
 
-// const Person = ()=>{
-//     return ( <p>Can put your code here. I am {Math.floor(Math.random()*30)} year old</p>
-//     );
-// }
+const Person = (props)=>{
+    return ( 
+    <div>
+    <p onClick={props.click}>Welcome. {props.name} is {props.age} year old</p>
+    <h3>{props.children}</h3>
+    </div>
+    );
 
-class Person extends Component{
-    render(){
-        return ( <p>Can put your code here. I am {Math.floor(Math.random()*30)} year old</p>
-        );
-    }
 }
+
+// class Person extends Component{
+//     render(){
+//         return ( <p>Can put your code here. I am {Math.floor(Math.random()*30)} year old</p>
+//         );
+//     }
+// }
 
 export default Person;
