28b03015cd35c0cec0fb5209de46ac81d26e2187
duy.pham <duy.pham@logixtek.com>
2020-11-05 16:52:22 +0700

    Working with css

--- FrontEndDemo/ReactJS/react-tutorials/src/App.js
+++ FrontEndDemo/ReactJS/react-tutorials/src/App.js
@@ -1,6 +1,6 @@
 import React,{useState} from "react";
 import "./App.css";
-import  Person from './components/Person';
+import  Person from './components/Person/Person'; 
 import  Example  from './components/Example';
 
 function App() {
---
+++ FrontEndDemo/ReactJS/react-tutorials/src/components/Person/Person.js
@@ -0,0 +1,36 @@
+// import React ,{Component} from "react";
+import React  from "react";
+import './Person.css';
+
+// const Person = (props)=>{
+//     return ( 
+//     <div className="Person">
+//     <p onClick={props.click}>Welcome. {props.name} is {props.age} year old</p>
+//     <h3>{props.children}</h3> 
+//     <input type="text" onChange={props.changed}></input>
+//     </div>
+//     );
+
+// }
+
+// class Person extends Component{
+//     render(){
+//         return ( <p>Can put your code here. I am {Math.floor(Math.random()*30)} year old</p>
+//         );
+//     }
+// }
+const Person = (props)=>{
+    const styles = {
+        color:'red',
+        fontWeight:'bold'
+    }
+    return ( 
+    <div className="Person">
+    <p style={styles} onClick={props.click}>Welcome. {props.name} is {props.age} year old</p>
+    <h3  >{props.children}</h3> 
+    <input type="text" onChange={props.changed}></input>
+    </div>
+    );
+
+};
+export default Person;
