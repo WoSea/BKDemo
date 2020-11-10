e8104cadfc2dfd638933b86542bfe5177a39771f
duy.pham <duy.pham@logixtek.com>
2020-11-10 15:01:54 +0700

    Adding Mediator Pattern Example

---
+++ MediatorExample/Components/BaseComponent.cs
@@ -0,0 +1,24 @@
+﻿using MediatorExample.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediatorExample.Components
+{
+    // The Base Component provides the basic functionality of storing a
+    // mediator's instance inside component objects.
+    class BaseComponent
+    {
+        protected IMediator _mediator;
+
+        public BaseComponent(IMediator mediator = null)
+        {
+            this._mediator = mediator;
+        }
+
+        public void SetMediator(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+    }
+}
---
+++ MediatorExample/Interfaces/IMediator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediatorExample.Interfaces
+{
+    // The Mediator interface declares a method used by components to notify the
+    // mediator about various events. The Mediator may react to these events and
+    // pass the execution to other components.
+    interface IMediator
+    {
+        void Notify(object sender, string ev);
+    }
+}
